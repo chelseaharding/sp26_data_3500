87abc67b1b5c0e1590fcaae5f8cd1b064bf56fef
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2026-01-21 21:39:27 +0000

    updating the ole lecture code

---
+++ lecture_code/week3.2.py
@@ -0,0 +1,90 @@
+# # dynamic types
+# x = 1
+# x = "aggie basketball"
+# x = True
+
+# print(type(x))
+
+# # input
+# age = input("Please enter your age: ")
+# print(type(age))
+
+# print("on your next birthday you will be", int(age) + 1)
+
+# # input example:
+# f_a_b_p = int(input("Enter the number of your favorite aggie basketball player: "))
+# print(type(f_a_b_p))
+
+# # casting 
+# # int()
+# # str()
+# # float()
+# # bool()
+# # eval() # tries to figure out the best type
+
+# # if
+# age = 12
+
+# if age > 20:
+#     print("The twenties are the best years of your life")
+# if age >= 13: # false
+#     print("You are a teenager. Have fun :)")
+# else:
+#     print("You are still a child :)")
+
+
+# if age >= 18:
+#     print("adult")
+# elif age >= 13:
+#     print("teenager")
+# else:
+#     print("child")
+
+# # hurd premium
+
+# student = input("Are you a current USU student: (Y/N)")
+# hurdP = input("Do you have Hurd Premium: (Y/N)")
+
+# if student == "Y":
+#     print("You get into the game free!")
+# if hurdP == "Y":
+#     print("You get into the game 15 minutes early!")
+# else:
+#     print("You need to buy a ticket. They're not too expensive!")
+
+# min, max, range
+
+# grades = [89, 56, 90, 0, 5]
+
+# print("min:", min(grades))
+# print("max:", max(grades))
+
+# range(10)
+
+
+# half marathon 
+"""
+Adam is running a half marathon. His avg mile time is 5:55.
+For the half (13.1), what is his projected finish time?
+Output the result in this format:
+"Time: hr:min:sec"
+"""
+
+minute = 5
+sec = 55
+pace_seconds = sec + (minute * 60)
+print("pace in seconds:", pace_seconds)
+
+total_half_sec = pace_seconds * 13.1
+print("total time in seconds for the half:", total_half_sec)
+
+projected_hours = total_half_sec // 3600
+# print("projected hours:", projected_hours)
+
+projected_minutes = (total_half_sec % 3600) // 60
+# print("projected minutes:", projected_minutes)
+
+projected_seconds = total_half_sec % 60
+# print("projected seconds:", projected_seconds)
+
+print("Time:", int(projected_hours), ":", int(projected_minutes), ":", projected_seconds)
--- programming_activities/week3.py
+++ programming_activities/week3.py
@@ -15,3 +15,27 @@ tax = 1.07
 total_bill = apple_price * num_purchased * tax
 
 print("you bought", num_purchased, "apples for", apple_price, "a piece. Your total bill was", total_bill)
+
+"""
+Programming Activity 2
+
+Write a program that asks the user how old they are, and what age they would like to live to. Calculate how long they have left to live (approximately), and then print a friendly message telling the user how long they have to live.
+"""
+age = int(input("Age: "))
+years_desired = int(input("How long would you like to live to: "))
+
+years_left = years_desired - age
+print("You will live for another", years_left, "years :)")
+
+"""
+Programming Activity 3
+
+Write a program that gets a user's score in this class, as a percentage i.e. 90 or 95. Write an if statement that checks to see if their score is equal to or greater than 93. If so, print "Congratulations you got an A" else print "Congratulations, you still learned a ton!!!!"
+"""
+
+score = int(input("Please enter your score: "))
+
+if score >= 93:
+    print("Congratulations you got an A")
+else:
+    print("Congratulations, you still learned a ton!!!!")
